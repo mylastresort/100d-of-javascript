efdd4ebe09d3a00cd143ca5da56ab90634980f1f
mylastresort <pmloik300@gmail.com>
2020-11-03 13:06:53 -0500

    just a demo

--- todo list/index.js
+++ todo list/index.js
@@ -1,17 +1,31 @@
-document.getElementById('adding').onclick=function() {
-    function gettingTheList() {
-        if (localStorage.getItem('TaskList')===null){
-            return [];
-        }
-        else{
-            return JSON.parse(localStorage.getItem('TaskList'))
-        }
-    }
-    var TaskList=gettingTheList()
-    var newTask=document.getElementById('newTask').value;
-    TaskList.push(newTask);
-    localStorage.setItem('TaskList',JSON.stringify(TaskList))
+// document.getElementById('adding').onclick=function() {
+//     function gettingTheList() {
+//         if (localStorage.getItem('TaskList')===null){
+//             return [];
+//         }
+//         else{
+//             return JSON.parse(localStorage.getItem('TaskList'))
+//         }
+//     }
+//     var TaskList=gettingTheList()
+//     var newTask=document.getElementById('newTask').value;
+//     TaskList.push(newTask);
+//     localStorage.setItem('TaskList',JSON.stringify(TaskList))
 
-};
+// };
 
 
+const today = new Date()
+
+const weekday = new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(today)
+const month = new Intl.DateTimeFormat('en-US', {month: 'long'}).format(today)
+const date = today.getDate()
+const year = today.getFullYear()
+
+const dayLabel = document.getElementById('day')
+console.log(weekday);
+dayLabel.textContent = weekday
+
+
+const dateLabel = document.getElementById('date')
+dateLabel.textContent = `${month} ${date}, ${year}`
