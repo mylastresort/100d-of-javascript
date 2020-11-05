ffefa6f100f8eb7a71442a6de827774152ed4499
mylastresort <pmloik300@gmail.com>
2020-11-05 17:12:36 -0500

    Adding more controls

--- notes app/index.js
+++ notes app/index.js
@@ -1,55 +1,69 @@
-var notes =localStorage.getItem('notes');
-
-var shown=false;
-
-if(notes){
-  var grids=JSON.parse(notes)
-}else{
-  var notes =new Array;
-  var notes= JSON.stringify(notes)
-  localStorage.setItem('notes',notes)
-}
-
+let section=document.querySelector(".notes-section");
 let add =document.getElementById('add');
-
 let typeArea=document.getElementById('typeArea');
-
-add.onclick=()=>{
-  plus()
+let iptcolor=document.getElementById('color')
+let iptsize=document.getElementById('select')
+iptsize.onchange=()=>{
+  typeArea.style.fontSize=iptsize.value
+}
+iptcolor.onchange=()=>{
+  typeArea.style.color=iptcolor.value
+}
+let notes=()=>{
+  if(!localStorage.getItem('notes') || localStorage.getItem('notes')==="[]"){
+    return [];
+  }else{
+    return JSON.parse(localStorage.getItem('notes'))
+  }
+}
+function Note(id,text,color,size){
+  this.id=id;
+  this.text=text;
+  this.color=color;
+  this.size=size;
+}
+function save(){
+  if(!!typeArea.value){
+    let notesUpdated=notes()
+    let index=notes().length
+    notesUpdated.push(new Note(index,typeArea.value,typeArea.style.color,typeArea.style.fontSize))
+    localStorage.setItem('notes',JSON.stringify(notesUpdated))
+  }
+  let newNotesArray=notes().slice(section.childNodes.length)
+  show(newNotesArray)
 }
 
-function plus(){
-  if(typeArea.value){
-    var value =typeArea.value;
-    notes=JSON.parse(notes);
-    notes.push(value);
-    notes=JSON.stringify(notes);
-    localStorage.setItem('notes',notes);
-    grids=JSON.parse(notes)
-  }
-  this.show(); //first time i use it lol!!
+function show(listOfNotes){
+  listOfNotes.map((i)=>{
+    let element=document.createElement('div')
+    let elementtxt=document.createTextNode(i.text)
+    element.appendChild(elementtxt)
+    element.style.fontSize=i.size;
+    element.style.color=i.color;
+    element.setAttribute('id',i.id)
+    element.classList='notes'
+    let deletebtn=document.createElement('button')
+    deletebtn.setAttribute('style', 'left: 94%; border-radius:30%; color:darkslategrey; position: absolute; width: 1.5vw; height: 2.5vh; font-size: 18px; background-color: #add8e654; outline: none; border: 0px; box-shadow: #add8e654 0px 0px 2px 3px;')
+    deletebtn.textContent='x';
+    deletebtn.setAttribute('onclick','deleteNote(this)')
+    element.appendChild(deletebtn)
+
+    section.appendChild(element)
+  })
 }
 
-let section=document.querySelector(".section");
+window.onload=()=>{
+  show(notes())
+}
 
-(function show(){
-  let appear=function (){
-    if(!shown){
-      for (const i in grids){
-        var element=document.createElement('div');
-        section.appendChild(element);
-        element.textContent=grids[i];
-        element.classList="notes";
-      }
-      shown=true;
-    }else{
-      var last=grids.length-1;
-      var element=document.createElement('div');
-      section.appendChild(element);
-      element.textContent=grids[last];
-      element.classList="notes";
-    }
+function deleteNote(e){
+  let noteIndex=parseInt(e.parentNode.id);
+  let notesUpdated=notes()
+  if(noteIndex===notesUpdated.length-1){
+    notesUpdated.pop()
+  }else{
+    notesUpdated.splice(noteIndex,1)
   }
-  appear()
-  window.show=appear;
-})();
+  localStorage.setItem('notes',JSON.stringify(notesUpdated))
+  e.parentNode.remove()
+}
