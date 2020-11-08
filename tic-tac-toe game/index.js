28628b60d37ac45a745c2b91ea36a310db1e83f8
mylastresort <pmloik300@gmail.com>
2020-11-08 11:17:27 -0500

    code cleaned

--- tic-tac-toe game/index.js
+++ tic-tac-toe game/index.js
@@ -1,3 +1,4 @@
+const cases=document.querySelectorAll('.case')
 ////showing a message: i'll show no mercy, you're okay with that?
 const xo={
   o:['o0.png','o1.png','o2.png'],
@@ -10,41 +11,45 @@ const inputs={
   reverseR:new Map()
 }
 
-let noob=[3,3]
-let easy=[5,4]      ///the first one means X*X grids, the second refer to how much Os/Xs is required to win
 
-let var1=easy[0]
-let var2=easy[1]
+let level={
+  noob:{
+    grids:3,
+    required:3
+  },
+  easy:{
+    grids:5,
+    required:4
+  }
+}
 
 let turn=0;
 
-const cases=document.querySelectorAll('.case')
-
-function dmeass(var1,var2){
-  const n=var1-var2;
+function genrateMemory(grids,required){
+  const n=grids-required;
   //n: to maitain every line/column possible if the user changed the number required cases to win
-  for(let i=0;i<var1;i++){
+  for(let i=0;i<grids;i++){
     inputs.lines.set(`L${i}`,new Map())
     inputs.colomuns.set(`C${i}`,new Map())
   }
   for(let p=0;p<n+1;p++){
-    for(let i=0;i<var1;i++){
+    for(let i=0;i<grids;i++){
       inputs.lines.get(`L${i}`).set(p,[])
       inputs.colomuns.get(`C${i}`).set(p,[])
     }
   }
   inputs.reverseL.set(`Rl0`,new Map())
   inputs.reverseR.set(`Rr0`,new Map())
-  for (let z=0;z<(var1-var2)*2;z++){
+  for (let z=0;z<(grids-required)*2;z++){
     inputs.reverseL.set(`Rl${z+1}`,new Map())
     inputs.reverseR.set(`Rr${z+1}`,new Map())
   }
-  for (let f=0;f<var1-var2+1;f++){
+  for (let f=0;f<grids-required+1;f++){
     inputs.reverseL.get(`Rl0`).set(f,[])
     inputs.reverseR.get(`Rr0`).set(f,[])
   }
-  let l=var1-var2;
-  let equation=var1-var2
+  let l=grids-required;
+  let equation=grids-required
   for(let j=0;j<l;j++){
     for(let o=0;o<equation;o++){
       inputs.reverseL.get(`Rl${j+1}`).set(o,[])
@@ -52,7 +57,7 @@ function dmeass(var1,var2){
     }
     equation--;
   }
-  equation=var1-var2
+  equation=grids-required
   for(let g=l;g<l*2;g++){
     for(let o=0;o<equation;o++){
       inputs.reverseL.get(`Rl${g+1}`).set(o,[])
@@ -63,7 +68,7 @@ function dmeass(var1,var2){
 }
 
 
-dmeass(var1,var2)
+genrateMemory(level.easy.grids, level.easy.required)
 
 
 
@@ -96,25 +101,26 @@ function play(element){
   let nRer=parseInt(id.split('-')[3][4])
   let lineRel=Rel.slice(0,3)
   let lineRer=Rer.slice(0,3)
-  ///assuming that this colomun its the last element on the line
-  //get the start line created on inputs from the colomun and it must not be negative 
+  ///assuming that this input is the last element on a line (line:lines i made on inputs object)
+  //get the start case possible on inputs objects (in case for lines:we grab it from the colomun) and it must not be negative 
   //for Lines
-  let key=nColomun-var2+1
+  let key=nColomun-required+1
   if(key<0){
     key=0
   }
-  //pushing the input statring from line 'key' on inputs
+  //pushing the input starting from line 'key' on inputs
+  ///example:if the nColomun is C2 and the required is 3 then the start is C0, the end is C[nColomun]
   for(let d=key;d<nColomun+1;d++){
     if(inputs.lines.get(line).get(d)!==undefined){
       inputs.lines.get(line).get(d).push(player)
     }
   }
   //////same for Colomuns
-  key=nLine-var2+1
+  key=nLine-required+1
   if(key<0){
     key=0
   }
-  //pushing the input on line 'key'
+  //pushing the input starting from line 'key' on inputs
   //////for reversed lines (leftside)
   for(let d=key;d<nLine+1;d++){
     if(inputs.colomuns.get(colomun).get(d)!==undefined){
@@ -122,11 +128,11 @@ function play(element){
     }
   }
   if(Rel!=='no'){
-    key=nRel-var2+1
+    key=nRel-required+1
     if(key<0){
       key=0
     }
-    //pushing the input on line 'key'
+  //pushing the input starting from line 'key' on inputs
   }
   for (let d=key;d<nRel+1;d++){
     if(inputs.reverseL.get(lineRel).get(d)!==undefined){
@@ -135,11 +141,11 @@ function play(element){
   }
   ///////for reversed lines (rightside)
   if(Rer!=='no'){
-    key=nRer-var2+1
+    key=nRer-required+1
     if(key<0){
       key=0
     }
-    //pushing the input on line 'key'
+    //pushing the input starting from line 'key' on inputs
     for(let d=key;d<nRer;d++){
       if(inputs.reverseR.get(lineRer).get(d)!==undefined){
         inputs.reverseR.get(lineRer).get(d).push(player)
@@ -147,24 +153,8 @@ function play(element){
     }
   }
 
-
   check()
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 function check(){
@@ -225,25 +215,6 @@ function check(){
     })
   })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // ////to make a demo
 // let i=0
