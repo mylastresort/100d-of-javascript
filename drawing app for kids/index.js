89eef901f2e6057fcef35f61a68c7f6dc9db4c9e
mylastresort <pmloik300@gmail.com>
2021-02-07 20:05:35 +0100

    Fixing the background image on html canvas

--- drawing app for kids/index.js
+++ drawing app for kids/index.js
@@ -1,14 +1,19 @@
-const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
+const canvas1 = document.querySelector('#canvas1');
+const context1 = canvas1.getContext('2d');
 
-canvas.width = 1000;
-canvas.height = 550;
+const canvas2 = document.querySelector('#canvas2');
+const context2 = canvas2.getContext('2d');
+
+canvas1.width = 1000;
+canvas1.height = 550;
+canvas2.width = 1000;
+canvas2.height = 550;
 
 const game = {
-  x:null,
-  y:null,
-  color:'purple',
-  pics:[
+  x: null,
+  y: null,
+  color: 'purple',
+  pics: [
     'https://www.cargamesonline.biz/html5/disney_planes_coloring_book/sprites/image1.png',
     'https://www.cargamesonline.biz/html5/disney_planes_coloring_book/sprites/image2.png',
     'https://www.cargamesonline.biz/html5/disney_planes_coloring_book/sprites/image3.png',
@@ -16,56 +21,49 @@ const game = {
     'https://www.cargamesonline.biz/html5/disney_planes_coloring_book/sprites/image5.png',
     'https://www.cargamesonline.biz/html5/disney_planes_coloring_book/sprites/image6.png',
   ],
-  drawing:false,
-  chosenPic:0,
-  raduis:10,
-  draw(context, x1, y1, x2, y2){
-    context.beginPath();
-    context.drawImage(image,0,0,canvas.width,canvas.height);
-    context.closePath();
+  drawing: false,
+  raduis: 10,
+  draw(context, x1, y1, x2, y2) {
     context.beginPath();
     context.lineJoin = 'round';
     context.lineCap = 'round';
     context.strokeStyle = this.color;
     context.lineWidth = this.raduis;
-    context.beginPath();
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
     context.stroke();
     context.closePath();
-
   },
   start(img) {
-    context.clearRect(0,0,canvas.width,canvas.height);
+    context2.clearRect(0,0,canvas2.width,canvas2.height);
+    context1.clearRect(0,0,canvas2.width,canvas2.height);
     image = new Image();
-    image.width = 100;
-    image.height = 50;
     image.src = game.pics[img];
     image.onload = _ => {
-      context.beginPath();
-      context.drawImage(image,0,0,canvas.width,canvas.height);
-      context.closePath();
+      context1.beginPath();
+      context1.drawImage(image,0,0,canvas2.width,canvas2.height);
+      context1.closePath();
     }
   }
 }
 
-canvas.onmousemove = _ => {
-  if(game.drawing) {
-    game.draw(context, game.x, game.y, _.offsetX, _.offsetY);
+canvas1.onmousemove = _ => {
+  if (game.drawing) {
+    game.draw(context2, game.x, game.y, _.offsetX, _.offsetY);
     game.x = _.offsetX;
     game.y = _.offsetY;
   }
 }
 
-canvas.onmousedown = _ => {
+canvas1.onmousedown = _ => {
   game.x = _.offsetX;
   game.y = _.offsetY;
   game.drawing = true;
 };
 
-canvas.onmouseup = _ => {
+canvas1.onmouseup = _ => {
   if (game.drawing) {
-    game.draw(context, game.x, game.y, _.offsetX, _.offsetY);
+    game.draw(context2, game.x, game.y, _.offsetX, _.offsetY);
     game.x = 0;
     game.y = 0;
     game.drawing = false;
@@ -73,13 +71,17 @@ canvas.onmouseup = _ => {
 };
 
 
-document.querySelector('.colors').onclick = _ => {
-  if(_.target.localName == 'span') game.color = window.getComputedStyle(_.target).backgroundColor;
+document.querySelector('.colors').onclick = _ => { if (_.target.localName == 'span') game.color = window.getComputedStyle(_.target).backgroundColor; }
+
+
+document.querySelector('.pics').onclick = _ => { if (_.target.localName == 'img') game.start(parseInt(_.target.id.slice(1))); }
+
+window.onload = _ => {
+  image = new Image();
+  image.src = game.pics[0];
+  image.onload = _ => {
+    context1.beginPath();
+    context1.drawImage(image, 0, 0, canvas2.width, canvas2.height);
+    context1.closePath();
+  }
 }
-
-
-document.querySelector('.pics').onclick = _ => {
-  if(_.target.localName == 'img') game.start(parseInt(_.target.id.slice(1)));
-}
-
-window.onload = _ => game.start(0)
