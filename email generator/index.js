3f46e5ccf4a9041c24d17ba12a6d4e347cffbe88
mylastresort <pmloik300@gmail.com>
2021-02-07 20:46:09 +0100

    code cleaned

--- email generator/index.js
+++ email generator/index.js
@@ -28,36 +28,36 @@ const ajax = (link, action, condition) => {
   xhr.onload = function () {
     if (this.status === 200) {
       try {
-        var data = JSON.parse(this.responseText)
+        var data = JSON.parse(this.responseText);
       } catch (error) {
         console.log('The Message was not found');
-        return
-      }
+        return;
+      };
       switch (action) {
         case 'generate':
-          email.value = data
-          localStorage.setItem('previous-email',JSON.stringify(data))
+          email.value = data;
+          localStorage.setItem('previous-email',JSON.stringify(data));
           break;
         case 'checkAll':
-          listId = new Array()
-          document.querySelectorAll('tr').forEach(e => listId.push(e.id))
+          listId = new Array();
+          document.querySelectorAll('tr').forEach(_ => listId.push(_.id));
           //showing the messages on the window
           data.forEach((element) => {
-            if (!listId.some(e => e == element.id)) {
-              let message = document.createElement('tr')
-              message.setAttribute('id', element.id)
+            if (!listId.some(_ => _ == element.id)) {
+              let message = document.createElement('tr');
+              message.setAttribute('id', element.id);
               for (let i = 0; i < 3; i++) {
-                let item = document.createElement('th')
-                item.setAttribute('id', 'items')
-                let itemtxt = document.createTextNode(Object.values(element)[i + 1])
-                item.appendChild(itemtxt)
-                message.appendChild(item)
+                let item = document.createElement('th');
+                item.setAttribute('id', 'items');
+                let itemtxt = document.createTextNode(Object.values(element)[i + 1]);
+                item.appendChild(itemtxt);
+                message.appendChild(item);
               }
-              let item = document.createElement('th')
+              let item = document.createElement('th');
               item.setAttribute('id', 'items')
-              item.innerHTML = '<i onclick="expand(this)" style="font-size:100%; opacity:0.7; cursor:pointer;" class="fas fa-angle-right"></i>';
-              message.appendChild(item)
-              table.appendChild(message)
+              item.innerHTML = '<i onclick="expand(this)" id="expand" style="font-size:100%; opacity:0.7; cursor:pointer;" class="fas fa-angle-right"></i>';
+              message.appendChild(item);
+              table.appendChild(message);
             }
           })
           break;
@@ -65,36 +65,49 @@ const ajax = (link, action, condition) => {
           document.querySelector('.text').childNodes[3].textContent = data.textBody;
           document.querySelector('.attachments').childNodes[3].textContent = data.attachments;
           break;
-      }
+      };
     } else {
-      alert('I did not reach the target')
+      alert('I did not reach the target');
     }
-  }
-  xhr.open('get', link, condition)
-  xhr.send()
+  };
+  xhr.open('get', link, condition);
+  xhr.send();
 }
+
+
 
 (async function () {
-  email.value = await JSON.parse(localStorage.getItem('previous-email'))
-  ajax(service.mailbox(), 'checkAll', true)
+  email.value = await JSON.parse(localStorage.getItem('previous-email'));
+  ajax(service.mailbox(), 'checkAll', true);
 })();
 
-setInterval(() => {
-  ajax(service.mailbox(), 'checkAll', false)
-}, 500);
+
 
 function expand(e) {
-  document.querySelector('.messages-section').style.marginLeft = '11%'
-  document.querySelector('.expand').style.width = '25vw';
-  let messageId = e.parentElement.parentElement.id;
-  ajax(service.mailbox(messageId), 'checkOne', true)
+  if(document.querySelector('.expand').style.width !== '25vw') {
+    document.querySelector('.messages-section').style.marginLeft = '11%';
+    document.querySelector('.expand').style.width = '25vw';
+    let messageId = e.parentElement.parentElement.id;
+    ajax(service.mailbox(messageId), 'checkOne', true);
+    document.getElementById('expand').style.transform = 'rotate(180deg)';
+  } else {
+    document.querySelector('.messages-section').style.marginLeft = '20%';
+    document.querySelector('.expand').style.width = '0vw';
+    document.getElementById('expand').style.transform = 'rotate(0deg)';
+  }
 }
 
+
 function copy() {
-  document.querySelector('#email').select()
-  document.execCommand('Copy')
+  document.querySelector('#email').select();
+  document.execCommand('Copy');
   document.getElementById('copied').style.opacity = 1;
   setTimeout(() => {
     document.getElementById('copied').style.opacity = 0;
   }, 1500);
 }
+
+
+setInterval(() => {
+  ajax(service.mailbox(), 'checkAll', false);
+}, 500);
